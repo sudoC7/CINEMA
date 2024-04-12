b3162bf7bc4a217427d0d38c1681e8cd7ddf4be2
SudoC7 <slowjacob7@gmail.com>
2024-04-12 16:11:37 +0200

    Display the movies

--- Projet/view/listFilms.php
+++ Projet/view/listFilms.php
@@ -5,7 +5,24 @@
     $title = "Films";
 ?>
 
+	<p>On a <?= $requeteFilms->rowCount() ?> films</p>
 
+	<table>
+		<thead>
+			<tr>
+				<th>TITRE</th>
+				<th>ANNEE SORTIE</th>
+			</tr>
+		</thead>
+		<tbody>
+			<?php foreach ($requeteFilms->fetchAll() as $film) { ?>
+				<tr>
+					<td><?= $film["titre"] ?></td>
+					<td><?= $film["anneeSortie"] ?></td>
+				</tr>
+			<?php }	?>
+		</tbody>
+	</table>
 
 
 <?php	
