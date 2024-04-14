53113a0d00ddcc4f88c8321cf1b2b2237e756c4e
SudoC7 <slowjacob7@gmail.com>
2024-04-14 15:03:22 +0200

    Adds the functions and display contents

--- Projet/view/listGenres.php
+++ Projet/view/listGenres.php
@@ -6,6 +6,25 @@
 ?>
 
 
+<h2>LES CATEGORIES</h2>
+	<!-- Compte le nombre de catégories du tableau  -->
+	<p>On a <?= $requeteGenres->rowCount() ?> catégories</p>
+
+	<table>
+		<thead>
+			<tr>
+				<th>Genres</th>
+			</tr>
+		</thead>
+		<tbody>
+			<!-- Affiche tous les genres du tableau  -->
+			<?php foreach ($requeteGenres->fetchAll() as $genre) { ?>
+				<tr>
+					<td><?= $genre["genreFilm"] ?></td>
+				</tr>
+			<?php }	?>
+		</tbody>
+	</table>
 
 
 <?php	
