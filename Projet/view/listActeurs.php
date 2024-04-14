4a28b6cf91aad36f10ce67a751514de20cd25f66
SudoC7 <slowjacob7@gmail.com>
2024-04-14 14:48:07 +0200

    Adds a new functions and display the content

--- Projet/view/listActeurs.php
+++ Projet/view/listActeurs.php
@@ -6,6 +6,29 @@
 ?>
 
 
+<h2>LES ACTEURS</h2>
+	<!-- Compte le nombre de realisateurs  -->
+	<p>On a <?= $requeteActeurs->rowCount() ?> acteurs</p>
+
+	<table>
+		<thead>
+			<tr>
+				<th>Nom</th>
+				<th>Prenom</th>
+				<th>Sexe</th>
+			</tr>
+		</thead>
+		<tbody>
+			<!-- Affiche tous les films du tableau  -->
+			<?php foreach ($requeteActeurs->fetchAll() as $acteur) { ?>
+				<tr>
+					<td><?= $acteur["nom"] ?></td>
+					<td><?= $acteur["prenom"] ?></td>
+					<td><?= $acteur["sexe"] ?></td>
+				</tr>
+			<?php }	?>
+		</tbody>
+	</table>
 
 
 <?php	
