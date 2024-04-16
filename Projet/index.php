291cce8bc2921a04b2e4fe32f0db3025e4085c1e
SudoC7 <slowjacob7@gmail.com>
2024-04-16 17:01:41 +0200

    small modif

--- Projet/index.php
+++ Projet/index.php
@@ -25,7 +25,7 @@ if(isset($_GET["action"])) {
         //===Films
         case "listFilms" : $ctrlFilm->listFilms(); break; 
         // detail film 
-        case "detailFilm" : $ctrlFilm->detailsFilm($id); break; 
+        case "detailFilm" : $ctrlFilm->detailFilm($id); break; 
 
 
         //===Acteurs
