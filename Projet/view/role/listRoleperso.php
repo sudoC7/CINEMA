dcedeec1fb00fb9c1a9ac7ead641657b39c23cb2
SudoC7 <slowjacob7@gmail.com>
2024-04-16 17:06:07 +0200

    Display the list role actor

---
+++ Projet/view/role/listRoleperso.php
@@ -0,0 +1,33 @@
+<?php
+	session_start();
+	ob_start();
+
+    $title = "Rôle personnage";
+?>
+
+
+	<h2>LES ROLES PERSONNAGE</h2>
+	<!-- Compte le nombre de realisateurs  -->
+	<p>Il y'a <?= $requeteRolePerso->rowCount(); ?> role personnage</p>
+
+	<table>
+		<thead>
+			<tr>
+				<th>Nom Personnage</th>
+			</tr>
+		</thead>
+		<tbody>
+			<!-- Affiche tous les réalisateurs du tableau -->
+			<?php foreach ($requeteRolePerso->fetchAll() as $perso) { ?>
+				<tr>
+					<td><?= $perso["nomPerso"] ?></td>
+				</tr>
+			<?php }	?>
+		</tbody>
+	</table>
+
+
+<?php	
+	$content = ob_get_clean();
+	require_once "view/template.php";
+?>
