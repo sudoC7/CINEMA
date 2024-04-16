4982b7b732477d472f1d38d2fa190a300a44261c
SudoC7 <slowjacob7@gmail.com>
2024-04-16 11:05:37 +0200

    add new function for detail movie

--- Projet/controller/FilmController.php
+++ Projet/controller/FilmController.php
@@ -22,7 +22,25 @@
 
         //Détails d'un Film 
         
-        public function detailsFilm() {}
+        public function detailsFilm() {
+
+            if(isset($_GET[$id])) {
+		
+                $rocketDetailsFilm = "SELECT titre, resumeFilm, noteFilm, duree, anneeSortie, CONCAT(realisateur.nomReal, ' ', realisateur.prenomReal) AS realisateur, CONCAT(acteur.nom, ' ', acteur.prenom) AS acteur, nomPerso AS roleActeur 
+                FROM acteur  	
+                INNER JOIN jouerole ON acteur.id_acteur = jouerole.id_acteur  
+                INNER JOIN roleperso ON jouerole.id_role_personnage = roleperso.id_roleperso  
+                INNER JOIN film ON film.id_film = jouerole.id_film  
+                INNER JOIN realisateur ON film.id_realisateur = realisateur.id_realisateur  
+                WHERE film.id_film = :id";
+                
+                $pdoDetailsFilm = Connect::seConnecter();
+                $rocketDetailsFilm->execute(["id" => $id]);
+                $recette = $rocketDetailsFilm->fetch();
+        
+
+
+        }
 
         //Ajouter un Film 
 
