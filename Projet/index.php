aa03909e6e6697d95fc7d7bbc4ae08e105f90a5d
SudoC7 <slowjacob7@gmail.com>
2024-04-16 16:25:02 +0200

    add the new instructions

--- Projet/index.php
+++ Projet/index.php
@@ -10,7 +10,7 @@ spl_autoload_register(function($class_name){
     include $class_name . '.php';
 });
 
-//$id = (isset($_GET["id"])) ? $_GET("id") : null;
+$id = (isset($_GET["id"])) ? $_GET("id") : null;
 
 $ctrlFilm = new FilmController();
 $ctrlActeur = new ActeurController();
@@ -21,10 +21,34 @@ $ctrlRoleperso = new RolepersoController();
 
 if(isset($_GET["action"])) {
     switch ($_GET["action"]) {
+
+        //===Films
         case "listFilms" : $ctrlFilm->listFilms(); break; 
+        // detail film 
+        case "detailFilm" : $ctrlFilm->detailsFilm($id); break; 
+
+
+        //===Acteurs
         case "listActeurs" : $ctrlActeur->listActeurs(); break;
+        // detail acteur 
+        case "listActeurs" : $ctrlActeur->detailActeur($id); break;
+
+
+        //===Genres
         case "listGenres" : $ctrlGenre->listGenres(); break;
+        // detail de genre 
+        case "detailGenre" : $ctrlGenre->detailGenre($id); break;
+
+
+        //===Realisateurs
         case "listRealisateurs" : $ctrlRealisateur->listRealisateurs(); break;
-        case "listRole" : $ctrlRoleperso->listRole($id);
+        // detail de realisateur 
+        case "detailRealisateur" : $ctrlRealisateur->detailRealisateur($id); break;
+
+
+        //===Role Personnage
+        case "listRoleperso" : $ctrlRoleperso->listRoleperso();
+        // detail de role perso
+        case "detailRoleperso" : $ctrlRoleperso->detailRolePerso($id);
     }
 }
