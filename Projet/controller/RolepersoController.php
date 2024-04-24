ff10b2756836c88e78ba9a7693bc08f3620d2f69
SudoC7 <slowjacob7@gmail.com>
2024-04-24 23:49:59 +0200

    Add the new function for displaying the detailRolePerso

--- Projet/controller/RolepersoController.php
+++ Projet/controller/RolepersoController.php
@@ -11,7 +11,7 @@
 
         public function listRoleperso() {
 
-            $rocketRole = "SELECT nomPerso FROM roleperso";
+            $rocketRole = "SELECT id_roleperso, nomPerso FROM roleperso";
 
             $pdoRolePerso = Connect::seConnecter();
             $requeteRolePerso = $pdoRolePerso->query($rocketRole);
@@ -21,7 +21,46 @@
         
         
         //Details des RolePersos
-        public function detailRolePerso() {}
+        public function detailRolePerso($id) {
+
+            $pdoRP1 = Connect::seConnecter();
+            $pdoRP = Connect::seConnecter();
+
+            if(isset($_GET['id'])) {
+                
+                // !!!!  REVOIR LA REQUETE "$roqueteRolePersoFilms" ( Affiche les mauvais films et acteur sur certains ??)!!! 
+
+                //Afficher le Role Personnage 
+    
+                $roqueteRolePerso = "SELECT id_roleperso, nomPerso
+                FROM roleperso 
+                WHERE id_roleperso = :id";
+
+                $pdoDetailRolePerso = $pdoRP1->prepare($roqueteRolePerso);
+                $pdoDetailRolePerso->execute(["id" => $id]);
+                $requeteRP = $pdoDetailRolePerso->fetch();
+                    
+    
+                //Afficher les films et l'acteur du Personnage 
+    
+                $roqueteRolePersoFilms = " SELECT film.titre, CONCAT(acteur.nom, ' ', acteur.prenom) AS Acteur
+                FROM acteur
+                INNER JOIN jouerole ON acteur.id_acteur = jouerole.id_acteur
+                INNER JOIN roleperso ON jouerole.id_role_personnage = roleperso.id_roleperso
+                INNER JOIN film ON film.id_film = jouerole.id_film
+                WHERE jouerole.id_acteur = :id"; 
+
+                $pdoDetailRolePersoFilms = $pdoRP->prepare($roqueteRolePersoFilms);
+                $pdoDetailRolePersoFilms->execute(["id" => $id]);
+                $requeteFilmRolePerso = $pdoDetailRolePersoFilms->fetchAll();
+
+
+            } else {
+                echo "Pas de contenu\n";
+            }
+
+            require "view/role/detailRoleperso.php";
+        }
         
         
         //Ajouter un RolePerso
