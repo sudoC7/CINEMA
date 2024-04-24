4c68712dc60d1c81644e1275f14d33b2f0051d97
SudoC7 <slowjacob7@gmail.com>
2024-04-24 10:44:20 +0200

    completed the function detailFilm()

--- Projet/controller/FilmController.php
+++ Projet/controller/FilmController.php
@@ -24,18 +24,29 @@
         public function detailFilm($id) {
 
             $pdoFilm = Connect::seConnecter();
-		
-            $rocketDetailsFilm = "SELECT titre, resumeFilm, noteFilm, duree, anneeSortie, CONCAT(realisateur.nomReal, ' ', realisateur.prenomReal) AS realisateur, CONCAT(acteur.nom, ' ', acteur.prenom) AS acteur, nomPerso AS roleActeur 
-            FROM acteur  	
-            INNER JOIN jouerole ON acteur.id_acteur = jouerole.id_acteur  
-            INNER JOIN roleperso ON jouerole.id_role_personnage = roleperso.id_roleperso  
-            INNER JOIN film ON film.id_film = jouerole.id_film  
-            INNER JOIN realisateur ON film.id_realisateur = realisateur.id_realisateur  
-            WHERE film.id_film = :id";
+
+            if(isset($_GET['id'])) {
+
+                $rocketDetailsFilm = "SELECT titre, resumeFilm, noteFilm, duree, anneeSortie, CONCAT(realisateur.nomReal, ' ', realisateur.prenomReal) AS realisateur, CONCAT(acteur.nom, ' ', acteur.prenom) AS acteur, nomPerso AS roleActeur 
+                FROM acteur  	
+                INNER JOIN jouerole ON acteur.id_acteur = jouerole.id_acteur  
+                INNER JOIN roleperso ON jouerole.id_role_personnage = roleperso.id_roleperso  
+                INNER JOIN film ON film.id_film = jouerole.id_film  
+                INNER JOIN realisateur ON film.id_realisateur = realisateur.id_realisateur  
+                WHERE film.id_film = :id";
+                    
+                $pdoDetailsFilm = $pdoFilm->prepare($rocketDetailsFilm);
+                $pdoDetailsFilm->execute(["id" => $id]);
+                $requeteFilm = $pdoDetailsFilm->fetchAll();
                 
-            $pdoDetailsFilm = $pdoFilm->prepare($rocketDetailsFilm);
-            $rocketDetailsFilm->execute(["id" => $id]);
-            $requeteFilm = $rocketDetailsFilm->fetchAll();
+            } else {
+                echo "Pas de contenu\n";
+            }
+            
+            require "view/film/detailFilm.php";
+            
+            // $requeteFilm = $pdo->prepare("SELECT * FROM film WHERE id_film = :id"); $requeteFilm->execute(["id"=> $id]) 
+          
                     
         }
         
