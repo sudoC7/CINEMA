fc64e7ebeed1267ec6b14cd4533a5fc02b6f1e95
SudoC7 <slowjacob7@gmail.com>
2024-04-24 16:08:36 +0200

    ad the new rocket for display the movie of actor

--- Projet/controller/ActeurController.php
+++ Projet/controller/ActeurController.php
@@ -22,21 +22,41 @@
         // Afficher les détails d'un acteur 
         public function detailActeur($id) {
 
+            $pdoActeur1 = Connect::seConnecter();
             $pdoActeur = Connect::seConnecter();
 
             if(isset($_GET['id'])) {
 
-                $rocketDetailActeur = "";
+                // Affiche un acteur en particulier 
+
+                $rocketActeur1 = "SELECT id_acteur, CONCAT(nom, ' ' , prenom) AS Acteur, sexe 
+                FROM acteur
+                WHERE acteur.id_acteur = :id";
+
+                $pdoDetailActeur1 = $pdoActeur1->prepare($rocketActeur1);
+                $pdoDetailActeur1->execute(["id" => $id]);
+                $requeteActeur1 = $pdoDetailActeur1->fetch();
+
+
+                //Affiche les films de l'acteur 
+
+                $rocketDetailActeur = "SELECT acteur.id_acteur, film.titre, roleperso.nomPerso, film.anneeSortie
+                FROM acteur
+                INNER JOIN jouerole ON acteur.id_acteur = jouerole.id_acteur
+                INNER JOIN roleperso ON jouerole.id_role_personnage = roleperso.id_roleperso
+                INNER JOIN film ON film.id_film = jouerole.id_film
+                WHERE acteur.id_acteur = :id";
 
                 $pdoDetailActeur = $pdoActeur->prepare($rocketDetailActeur);
                 $pdoDetailActeur->execute(["id" => $id]);
                 $requeteActeur = $pdoDetailActeur->fetchAll();
 
+
             } else {
                 echo "Pas de contenu\n";
             }
 
-            require "view/acteur/listActeurs.php";
+            require "view/acteur/detailActeur.php";
         }
         
         // Ajouter un Acteur 
