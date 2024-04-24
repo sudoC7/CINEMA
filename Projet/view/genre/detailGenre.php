ae58e13be87e8f8bbe19169298ac84356adfad5c
SudoC7 <slowjacob7@gmail.com>
2024-04-24 22:40:31 +0200

    content for display the movie

---
+++ Projet/view/genre/detailGenre.php
@@ -0,0 +1,32 @@
+<?php
+	session_start();
+	ob_start();
+
+    $title = "Genre";
+?>
+
+
+<h2>LES FILMS DU CATEGORIE :  <?= $requeteGenre['genreFilm'] ?></h2>
+	<!-- Compte le nombre de catégories du tableau  -->
+
+	<table>
+		<thead>
+			<tr>
+				<th>LISTE FILMS</th>
+			</tr>
+		</thead>
+		<tbody>
+			<!-- Affiche tous les genres du tableau  -->
+			<?php foreach ($requeteGenreFilms as $film) { ?>
+				<tr>
+					<td><a href=""><?= $film["titre"] ?></a></td>
+				</tr>
+			<?php }	?>
+		</tbody>
+	</table>
+
+
+<?php	
+	$content = ob_get_clean();
+	require_once "view/template.php";
+?>
