902b0893b617a281b32b37b5a9bb9ae7852770bd
SudoC7 <slowjacob7@gmail.com>
2024-04-24 22:39:55 +0200

    Add the new function for detail type movie

--- Projet/controller/GenreController.php
+++ Projet/controller/GenreController.php
@@ -11,7 +11,7 @@
 
         public function listGenres() {
 
-            $rocketGenres ="SELECT genreFilm FROM genre";
+            $rocketGenres ="SELECT id_genre, genreFilm FROM genre";
         
             $pdoGenres = Connect::seConnecter();
             $requeteGenres = $pdoGenres->query($rocketGenres);
@@ -19,8 +19,41 @@
         }
 
 
-        // Lister les films par genre 
-        public function listFilmGenre() {}
+        // Detail d'un genre 
+        public function detailGenre($id) {
+
+            $pdoGenre1 = Connect::seConnecter();
+            $pdoGenre = Connect::seConnecter();
+
+            if(isset($_GET['id'])){
+
+                // Requete pour afficher le genre particulier
+                $rocketGenre = "SELECT id_genre, genreFilm
+                FROM genre
+                WHERE id_genre = :id ";
+
+                $pdoGenreParticulier = $pdoGenre1->prepare($rocketGenre);
+                $pdoGenreParticulier->execute(["id" => $id]);
+                $requeteGenre = $pdoGenreParticulier->fetch();
+
+                // Requete pour afficher tous les films du genre choisi
+                $rocketGenreFilms = "SELECT genre.id_genre, titre
+                FROM genre
+                INNER JOIN categorie ON categorie.id_genre = genre.id_genre
+                INNER JOIN film ON categorie.id_film = film.id_film
+                WHERE genre.id_genre = :id";
+
+                $pdoGenreFilms = $pdoGenre->prepare($rocketGenreFilms);
+                $pdoGenreFilms->execute(["id" => $id]);
+                $requeteGenreFilms = $pdoGenreFilms->fetchAll();
+
+
+            } else {
+                echo "Pas de contenu\n";
+            }
+
+            require "view/genre/detailGenre.php";
+        }
 
         //Ajout d'un genre
         public function ajoutGenre() {}
