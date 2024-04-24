6ef8ba42fd49da428da3e9e55dd7c5e3b7812fc5
SudoC7 <slowjacob7@gmail.com>
2024-04-24 16:10:05 +0200

    modified the content for display

--- Projet/view/film/detailFilm.php
+++ Projet/view/film/detailFilm.php
@@ -7,33 +7,25 @@
 
 <!-- Ici nous allons afficher le detail d'un film ( titre acteur reéalisateur role synopsis etc...)-->
 
-<h2>DETAILS DU film </h2>
+<h2>DETAILS DU film : <?= $requeteFilmtitre["titre"] ?> </h2>
 	<!-- Compte le nombre de realisateurs  -->
+	<p><?= $requeteFilmtitre["resumeFilm"] ?></p>
+	<p><?= $requeteFilmtitre["noteFilm"] ?></p>
+	<p><?= $requeteFilmtitre["duree"] ?></p>
+	<p><?= $requeteFilmtitre["anneeSortie"] ?></p>
+	<p><?= $requeteFilmtitre["Realisateur"] ?></p>
 
 	<table>
 		<thead>
 			<tr>
-				<th>TITRE</th>
-				<th>SYNOPSIS</th>
-				<th>NOTE</th>
-				<th>DUREE</th>
-				<th>anneeSortie</th>
-				<th>REALISATEUR</th>
 				<th>ACTEUR</th>
 				<th>ROLE</th>
 			</tr>
 		</thead>
 		<tbody>
-
 			<!-- Affiche tous les films du tableau  -->
 			<?php foreach ($requeteFilm as $film) { ?>
-				<tr>
-					<td><?= $film["titre"] ?></td>
-					<td><?= $film["resumeFilm"] ?></td>
-					<td><?= $film["noteFilm"] ?></td>
-					<td><?= $film["duree"] ?></td>
-					<td><?= $film["anneeSortie"] ?></td>
-					<td><?= $film["realisateur"] ?></td>
+					<tr>
 					<td><?= $film["acteur"] ?></td>
 					<td><?= $film["roleActeur"] ?></td>
 				</tr>
