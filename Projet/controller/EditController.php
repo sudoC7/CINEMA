aabc5b47c1c58dbbc49e1c4e03f96608b8ed37ec
SudoC7 <slowjacob7@gmail.com>
2024-04-28 20:50:54 +0200

    controller

---
+++ Projet/controller/EditController.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Controller;
+use Model\Connect;
+
+    // Dans cette page je vais inclure les fonctions de supprimer des autres controller pour les envoyer dans "editCinema.php"
+    // 
+
+?>
