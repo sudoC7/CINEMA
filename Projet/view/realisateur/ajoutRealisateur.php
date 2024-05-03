4463548733e87e7b9b5ad4f2c57870605d5f02dd
SudoC7 <slowjacob7@gmail.com>
2024-05-03 17:52:05 +0200

    completed the formulary for add the new "Realisateur"

--- Projet/view/realisateur/ajoutRealisateur.php
+++ Projet/view/realisateur/ajoutRealisateur.php
@@ -7,27 +7,39 @@
 
 
     <!--     id_realisateur, nomReal, prenomReal, dateNaissanceReal, afficheReal   -->
-    <form action="index.php***">
+    <form action="index.php?action=ajoutRealisateur" method="post" enctype="multipart/form=data">
         
-        <label for="">
-            <p></p>
-            <input type="text">
+        <label>
+            <p>Nom</p>
+            <input type="text" name="lastname">
         </label>
-        <label for="">
-            <p></p>
-            <input type="text">
+        <label>
+            <p>Prenom</p>
+            <input type="text" name="firstname">
         </label>
-        <label for="">
-            <p></p>
-            <input type="text">
+
+        <!-- pour la date de naissance -->
+        <label>
+            <p>Date de naissance</p>
+            <input type="date" name="bday" value="0000-00-00">
         </label>
-        <label for="">
-            <p></p>
-            <input type="text">
+
+        <!-- pour importer une photo -->
+        <label>
+            <p>photo</p>
+            <input type="file" name="fileImg" accept=".jpg, .png, .jpeg, .svg" >
+            <!--<button type="submit" name="fileImg">UPLOAD</button>-->
         </label>
+
+        <!-- Boutton submit du fomulaireo -->
+        <label>
+            <br><input type="submit" name="submit" value="Ajouter">
+        </label>
+        
         
     </form>
 
+    <a href="index.php?action=listEditCinema">Retour</a>
 
 <?php	
 	$content = ob_get_clean();
