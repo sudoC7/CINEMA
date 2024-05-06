b56bb0b8ccc759efca5d1e1b30c0ae762492721b
SudoC7 <slowjacob7@gmail.com>
2024-05-06 11:25:38 +0200

    del the label tag for uploading the file

--- Projet/view/realisateur/ajoutRealisateur.php
+++ Projet/view/realisateur/ajoutRealisateur.php
@@ -1,46 +1,30 @@
 <?php
 	session_start();
 	ob_start();
-
-    $title = "Ajout de Réalisateur";
+    $title = "Ajout de Réalisateur"; 
 ?>
 
-
     <!--     id_realisateur, nomReal, prenomReal, dateNaissanceReal, afficheReal   -->
-    <form action="index.php?action=ajoutRealisateur" method="post" enctype="multipart/form=data">
+    <form action="index.php?action=ajoutRealisateur" method="POST" enctype="multipart/form-data">
         
-        <label>
             <p>Nom</p>
             <input type="text" name="lastname">
-        </label>
-        <label>
             <p>Prenom</p>
             <input type="text" name="firstname">
-        </label>
-
+        
         <!-- pour la date de naissance -->
-        <label>
             <p>Date de naissance</p>
             <input type="date" name="bday" value="0000-00-00">
-        </label>
-
-        <!-- pour importer une photo -->
-        <label>
-            <p>photo</p>
-            <input type="file" name="fileImg" accept=".jpg, .png, .jpeg, .svg" >
-            <!--<button type="submit" name="fileImg">UPLOAD</button>-->
-        </label>
-
-        <!-- Boutton submit du fomulaireo -->
-        <label>
-            <br><input type="submit" name="submit" value="Ajouter">
-        </label>
         
+        <!-- pour importer une photo -->
+            <p>Photo</p>
+            <input type="file" name="fileImg">
+        
+        <!-- Boutton submit du fomulaire -->
+            <input type="submit" name="submit" value="Ajouter">
         
     </form>
-
     <a href="index.php?action=listEditCinema">Retour</a>
-
 <?php	
 	$content = ob_get_clean();
 	require_once "view/template.php";
