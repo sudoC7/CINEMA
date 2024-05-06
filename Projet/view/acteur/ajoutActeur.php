2c9a4ba290387f4110d01cd36039d4e5292d1dd5
SudoC7 <slowjacob7@gmail.com>
2024-05-06 16:53:40 +0200

    Modified the form

--- Projet/view/acteur/ajoutActeur.php
+++ Projet/view/acteur/ajoutActeur.php
@@ -4,41 +4,26 @@
 
     $title = "Ajout d'Acteur";
 ?>
-
+    <h2>Ajout d'Acteur</h2>
      <!--     id_acteur, nom, prenom, sexe, dateNaissance, afficheActeur    -->
-     <form action="index.php***" method="post" enctype="multipart/form-data">
-        
-        <!--nom -->
-        <label for="">
-            <p></p>
-            <input type="text">
-        </label>
-        
-        <!--prenom -->
-        <label for="">
-            <p></p>
-            <input type="text">
-        </label>
-        
-        <!--sexe -->
-        <label for="">
-            <p></p>
-            <input type="text">
-        </label>
-        
-        <!--dateNaissance -->
-        <label for="">
-            <p></p>
-            <input type="text">
-        </label>
+     <form action="index.php?action=ajoutActeur" method="post" enctype="multipart/form-data">
+
+            <p>Nom</p>
+            <input type="text" name="lastName" >
+            <p>Prenom</p>
+            <input type="text" name="firstName" >
+            <p>Sexe</p>
+            <input type="text" name="sexe" >
+            <p>Date de naissance</p>
+            <input type="date" name="bday" value="0000-00-00">
         
         <!--afficheActeur -->
-        <label for="">
-            <p></p>
-            <input type="text">
-        </label>
-        
-    </form>
+            <p>Photo</p>
+            <input type="file" name="fileImg" accept=".jpg, .jpeg, .png, .svg">
+
+        <!--Boutton d'ajout -->
+            <input type="submit" name="submit" value="Ajouter">
+     </form>                                                              
 
     <a href="index.php?action=listEditCinema">Retour</a>
 
