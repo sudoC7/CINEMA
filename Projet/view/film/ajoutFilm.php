8d928ed74d3a03d0d8bd610bf8b83c8dab897228
SudoC7 <slowjacob7@gmail.com>
2024-05-07 16:52:15 +0200

    completed form

--- Projet/view/film/ajoutFilm.php
+++ Projet/view/film/ajoutFilm.php
@@ -8,37 +8,37 @@
     <!--     id_film, titre, anneeSortie, duree, resumeFilm, noteFilm, afficheFilm, afficheBack, id_realisateur    -->
     <form action="index.php?action=ajoutFilm" method="post" enctype="multipart/form-data">
             
-        <!--Titre -->
-            <p>Titre</p>
-            <input type="text">
-            
-            
+        
+            <!--Titre -->
+                <p>Titre</p>
+                <input type="text" name="titre">
+             
             <!-- anneeSortie -->
                 <p>Annee de sortie</p>
-                <input type="text" name="titre">
+                <input type="number" name="anneeSortie">
             
-            <!--duree(min) -->
+            <!-- duree(min) -->
                 <p>Durée</p>
-                <input type="number" name="">
+                <input type="number" name="duree">
             
-            <!--synopsis(resumeFilm) -->
+            <!-- synopsis(resumeFilm) -->
                 <p>Synopsis</p>
                 <input type="text" name="resumeFilm">
             
-            <!--noteFilm -->
+            <!-- noteFilm -->
                 <p>Note</p>
                 <input type="number" name="noteFilm">
             
-            <!--afficheFilm et afficheBack -->
+            <!-- afficheFilm et afficheBack 
                 <p>Photo principal</p>
                 <input type="file" name="afficheFilm">
                 <p>Photo du Back</p>
-                <input type="file" name="afficheBack">
+                <input type="file" name="afficheBack">-->
             
-            <!-- id_realisateur (choix de réalisateur)  Faire un foreach qui la lister tous les réalisateur et pouvoir choisir le réalisateur-->
-                <p>Réalisateur</p>
-            
-                <input type="text">
+            <!-- id_realisateur (choix de réalisateur)  Faire un foreach qui la lister tous les réalisateur et pouvoir choisir le réalisateur -->
+              
+            <!-- Boutton pour ajouter -->    
+                <input type="submit" name="submit" value="Ajouter">
             
     </form>
 
